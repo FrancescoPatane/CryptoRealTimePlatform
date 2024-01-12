e04a21990138467372b974838bdc459207ac937e
PATANEF-NB\PATANEF <francesco.patane@linksmt.it>
2024-01-13 00:35:44 +0100

    real time manager project

---
+++ RealTimeManager/src/main/java/it/fp/crypto/rtp/rtmanager/RtmanagerApplication.java
@@ -0,0 +1,13 @@
+package it.fp.crypto.rtp.rtmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RtmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RtmanagerApplication.class, args);
+	}
+
+}
---
+++ RealTimeManager/src/test/java/it/fp/crypto/rtp/rtmanager/RtmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package it.fp.crypto.rtp.rtmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RtmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
